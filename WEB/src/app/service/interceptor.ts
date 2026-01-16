9233456ff0bd2b43d9625bf0faf35f8678a87b88
ThePepi1 <122203922+ThePepi1@users.noreply.github.com>
2026-01-16 13:59:38 +0100

    Configure HTTP interceptor to use updated token key for authorization

--- WEB/src/app/service/interceptor.ts
+++ WEB/src/app/service/interceptor.ts
@@ -6,7 +6,7 @@ import { Observable } from "rxjs";
 export class AuthInterceptor  implements  HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        const token = localStorage.getItem('authToken') || sessionStorage.getItem('authToken');
+        const token = localStorage.getItem('authTokenUser') || sessionStorage.getItem('authTokenUser');
         
         if (token) {
             const cloned = req.clone({

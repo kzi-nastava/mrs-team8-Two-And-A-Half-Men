52039dd0366ec5727030d7e82f3fcb0fb8c6e1c8
ThePepi1 <122203922+ThePepi1@users.noreply.github.com>
2026-01-17 10:58:36 +0100

    Implement cancellation feature with role-based handling and environment configuration

--- WEB/src/app/cancellation/cancellation-button/cancellation-button.ts
+++ WEB/src/app/cancellation/cancellation-button/cancellation-button.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import Swal from 'sweetalert2';
 import { CancelationService } from '../../service/cancelation';
+import { Auth } from '../../service/auth';
 @Component({
   selector: 'app-cancellation-button',
   imports: [],
@@ -9,46 +10,72 @@ import { CancelationService } from '../../service/cancelation';
 })
 export class CancellationButton {
 
-  constructor(private cancelationService: CancelationService) {}
+  constructor(private cancelationService: CancelationService, private auth: Auth) {}
 
   @Input() itemId ?: string|number;
     cancel(): void {
-      //implement role logic later
-        if(true) {
+      console.log(this.auth.getRole());
+        if(this.auth.getRole() == 'ROLE_DRIVER') {
           Swal.fire({
       title: 'Cancellation Reason',
-      input: 'textarea',
-      inputLabel: 'Please provide a reason for cancellation',
-      inputPlaceholder: 'Enter your reason here...',
-      inputAttributes: {
-        'aria-label': 'Enter your cancellation reason'
-      },
-      showCancelButton: true,
-      confirmButtonText: 'Cancel',
-      cancelButtonText: 'Close',
-      confirmButtonColor: '#dc3545',
-      cancelButtonColor: '#6c757d',
-      inputValidator: (value) => {
-        if (!value) {
-          return 'You need to provide a reason!';
-        }
-        return null;
-      }
-    }).then((result) => {
-      if (result.isConfirmed) {
-        const message = this.cancelationService.createCancellationRequest(result.value ?? '', this.itemId ?? '');
-        message.subscribe({
-          next: (response) => {
-             Swal.fire('Cancelled!', 'Your cancellation has been processed.', 'success');
-          },
-          error: (err) => {
-            Swal.fire('Error', 'There was an error processing your cancellation.' + err.message, 'error');
+            html:
+            `
+             <div style="text-align: left; width: 90%;">
+          <label style="display: block; margin-bottom: 10px; font-weight: bold;">
+            Cancelled by:
+          </label>
+          <select id="cancelledBy" class="swal2-select" style="width: 90%; margin-bottom: 15px;">
+            <option value="">Select...</option>
+            <option value="CUSTOMER">Customer issues</option>
+            <option value="DRIVER">Driver issues</option>
+          </select>
+          
+          <label style="display: block; margin-bottom: 10px; font-weight: bold;">
+            Reason:
+          </label>
+          <textarea 
+            id="cancellationReason" 
+            class="swal2-textarea" 
+            placeholder="Enter your reason here..." 
+            style="width: 90%; height: 300px; resize: none;"
+            aria-label="Enter your cancellation reason"></textarea>
+        </div>
+        `,
+        showCancelButton: true,
+        confirmButtonText: 'Cancel Ride',
+        cancelButtonText: 'Cancel',
+        confirmButtonColor: '#dc3545',
+        cancelButtonColor: '#6c757d',
+        preConfirm: () => {
+          const reason = (Swal.getPopup()?.querySelector('#cancellationReason') as HTMLTextAreaElement).value;
+          const cancelledBy = (Swal.getPopup()?.querySelector('#cancelledBy') as HTMLSelectElement).value;
+          if (!reason) {
+            return null;
           }
-        });
-       
-      }
-    });
-    }else {
+          if (!cancelledBy) {
+            return null;
+          }
+          return `${cancelledBy}: ${reason}`;
+        }
+      }).then((result) => {
+        if (result.isConfirmed) {
+          const cancelledBy = result.value.split(': ')[0];
+          const reason = result.value.split(': ')[1];
+          if (!reason) {
+            return;
+          }
+          const message = this.cancelationService.createCancellationRequest(reason, cancelledBy, this.itemId!);
+          message.subscribe({
+            next: () => {
+              Swal.fire('Cancelled!', 'The ride has been cancelled.', 'success');
+            },
+            error: (error) => {
+              Swal.fire('Error!', 'There was an error cancelling the ride. ' + error.message, 'error');
+            }
+          });
+        }
+      });
+    }else if(this.auth.getRole() == 'ROLE_CUSTOMER'){
     Swal.fire({ 
     
         title: 'Cancelation',
--- WEB/src/app/service/cancelation.ts
+++ WEB/src/app/service/cancelation.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environments';
 
 @Injectable({
   providedIn: 'root',
@@ -8,9 +9,9 @@ import { HttpClient } from '@angular/common/http';
 export class CancelationService {
   constructor(private http: HttpClient) {}
 
-  createCancellationRequest(reason: string, id: string | number): Observable<{ message: string | null }> {
+  createCancellationRequest(reason: string, cancelledBy: string, id: string | number): Observable<{ message: string | null }> {
 
-    return this.http.post<{ message: string | null }>(`http://localhost:8080/api/v1/rides/${id}/cancel`, { reason });
+    return this.http.post<{ message: string | null }>(`${environment.apiUrl}/v1/rides/${id}/cancel`, { reason, cancelledBy });
   }
   
 }

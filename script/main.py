25c666171d1212476993b14ab00961b27f28de55
ThePepi1 <122203922+ThePepi1@users.noreply.github.com>
2026-01-28 09:33:29 +0100

    refactor: replace hardcoded addresses with environment variables for route movement

--- script/main.py
+++ script/main.py
@@ -1,3 +1,9 @@
 import MovingInRoute
+import os
 
-MovingInRoute.move_along_route("Bulevar Despota Stefana 7a", "Fakultet tehničkih nauka, Novi Sad", "driver@test.com")
+startAddress = os.getenv("START_ADDRESS")
+endAddress = os.getenv("END_ADDRESS")
+userEmail = os.getenv("USER_EMAIL")
+userPassword = os.getenv("USER_PASSWORD")
+
+MovingInRoute.move_along_route(startAddress, endAddress, userEmail, userPassword)

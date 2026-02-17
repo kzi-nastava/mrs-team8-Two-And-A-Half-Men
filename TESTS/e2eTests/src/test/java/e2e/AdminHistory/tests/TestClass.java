381253a485154a2d3622687e3fa49c363ee9ecf4
ThePepi1 <122203922+ThePepi1@users.noreply.github.com>
2026-02-17 17:36:28 +0100

    Add PanicService mock to RideCommandController and RideEndController tests; update TestClass to use BASE_URL for frontend URL

--- TESTS/e2eTests/src/test/java/e2e/AdminHistory/tests/TestClass.java
+++ TESTS/e2eTests/src/test/java/e2e/AdminHistory/tests/TestClass.java
@@ -9,17 +9,18 @@ import org.testng.annotations.*;
 
 
 public class TestClass {
-
+    protected static final String BASE_URL = System.getenv("BASE_URL");
+    private final String CHROME_VERSION = System.getenv("CHROME_VERSION");
     public String frontendUrl;
     public WebDriver driver;
     @BeforeClass
     @Parameters({"frontendUrl"})
     public void setup(String frontendUrl) {
-        WebDriverManager.chromedriver().browserVersion("144").setup();
+        WebDriverManager.chromedriver().browserVersion(CHROME_VERSION).setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--incognito");
         this.driver = new ChromeDriver(options);
-        this.frontendUrl = frontendUrl;
+        this.frontendUrl = BASE_URL;
         try {
             System.out.println("Resetting database before tests...");
             DatabaseResetUtility.resetDatabase();

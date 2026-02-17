609f9ba13c3e5e1a6fdc55ff52a90c2a14c7707d
stefanftn <stefanilic3001@gmail.com>
2026-02-17 19:12:42 +0100

    price changing feature implementation on mobile

--- MOBILE/app/src/main/java/com/project/mobile/service/VehicleService.java
+++ MOBILE/app/src/main/java/com/project/mobile/service/VehicleService.java
@@ -1,11 +1,27 @@
 package com.project.mobile.service;
 
+import com.project.mobile.DTO.vehicles.PricingUpdate;
 import com.project.mobile.DTO.vehicles.VehicleOptions;
+import com.project.mobile.DTO.vehicles.VehicleType;
+
+import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.PATCH;
+import retrofit2.http.Path;
 
 public interface VehicleService {
     @GET("vehicles/options")
     Call<VehicleOptions> getVehicleOptions();
+
+    @GET("vehicle-types")
+    Call<List<VehicleType>> getVehicleTypes();
+
+    @PATCH("vehicle-types/{id}/price")
+    Call<VehicleType> updateVehiclePrice(
+            @Path("id") long id,
+            @Body PricingUpdate pricingUpdate
+    );
 }
